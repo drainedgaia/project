ba377bfc72b1bd0a6c18c96fe29b5c5f67bfa8af
drainedgaia <drainedgaia@gmail.com>
2025-12-07 11:28:59 +0200

    refactor(model); use dataclasses to represent user

--- src/data/models.py
+++ src/data/models.py
@@ -1,12 +1,16 @@
-class User:
-    def __init__(self, first_name, last_name, email):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.gpa = None
-        self.courses = []
+from dataclasses import dataclass
+from typing import List
 
+@dataclass
 class Course:
-    def __init__(self, name, grade):
-        self.name = name
-        self.grade = grade
+    name: str
+    grade: str
+
+@dataclass
+class User:
+    first_name: str
+    last_name: str
+    email: str
+    gpa: float
+    courses: List[Course]
+
