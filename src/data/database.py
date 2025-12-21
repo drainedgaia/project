ba308f090b856d7c328b19eb712de21fb26c4b71
drainedgaia <drainedgaia@gmail.com>
2025-12-21 20:44:17 +0200

    feat(database): add database-related module

---
+++ src/data/database.py
@@ -0,0 +1,23 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Get the absolute path to the project root
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+db_path = os.path.join(project_root, "loginpage.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    from . import models
+    Base.metadata.create_all(bind=engine)
